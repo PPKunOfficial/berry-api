743faf6f0767acc4c123fbd11224fb13aa340dc6
PPKunOfficial <2967630235@qq.com>
2025-05-18 12:54:07 +0800

    chore: 更新 .gitignore，移除 service 目录，调整 Cargo.toml 和路由设置

--- api/src/router/api.rs
+++ api/src/router/api.rs
@@ -1,5 +1,13 @@
-use axum::{Router, routing::get};
+use axum::{
+    Router,
+    routing::{get, post},
+};
 
-pub async fn set_api_router() -> Router {
-    Router::new().route("/", get(|| async { "Api router" }))
+pub fn set_api_router() -> Router {
+    Router::new().nest("/user", set_user_router())
+}
+fn set_user_router() -> Router {
+    Router::new()
+        .route("/", get(|| async { "User router" }))
+        .route("/register", post(|| async { "User router" }))
 }
--- api/src/router/router.rs
+++ api/src/router/router.rs
@@ -1,12 +1,13 @@
 use axum::{Router, routing::get};
 use tower_http::trace::TraceLayer;
 
-use super::relay::set_relay_router;
+use super::{api::set_api_router, relay::set_relay_router};
 
 pub fn set_router() -> Router {
     Router::new()
         .route("/", get(index))
         .merge(set_relay_router())
+        .nest("/api", set_api_router())
         .layer(TraceLayer::new_for_http())
 }
 async fn index() -> &'static str {
